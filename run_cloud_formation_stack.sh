b7e4d230641c39339618d2d3cba61fff99f777b4
Lindy Lewis <lindylewis@lindys-imac.attlocal.net>
2017-12-22 15:58:29 -0600

    Added filepath to template and template

--- run_cloud_formation_stack.sh
+++ run_cloud_formation_stack.sh
@@ -1,4 +1,4 @@
 stack_name="$1"
 echo $stack_name
 
-aws cloudformation create-stack --stack-name "$stack_name"
+aws cloudformation create-stack --stack-name "$stack_name" --template-body file://ec2.json
