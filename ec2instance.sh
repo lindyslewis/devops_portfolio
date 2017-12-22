44c9e9f0ac3bc5a0f958c4d6bb79a3c7a27f4477
Lindy Lewis <lindylewis@lindys-imac.attlocal.net>
2017-12-22 15:37:06 -0600

    Corrected Subnet ID

--- ec2instance.sh
+++ ec2instance.sh
@@ -1,7 +1,7 @@
 echo "Hello from ec2instance.sh"
 echo "Now launching an EC2 instance..."
 
-aws ec2 run-instances --image-id ami-618f831a --count 1 --instance-type t2.micro --key-name LabEC2KeyPair --security-group-ids sg-f7311682 --subnet-id subnet-9ee8ace6
+aws ec2 run-instances --image-id ami-618f831a --count 1 --instance-type t2.micro --key-name LabEC2KeyPair --security-group-ids sg-f7311682 --subnet-id subnet-f25ce8dd
 
 
 
